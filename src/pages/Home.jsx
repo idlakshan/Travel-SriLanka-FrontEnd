2dab6e1bac1d0dd756df9bdf8aaf1bf9d1bc23ac
idlakshan <id.lakshan21@gmail.com>
2023-12-21 11:27:23 +0530

    implement experince section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,7 @@ import { Container, Row, Col } from 'reactstrap'
 import homeImg1 from './../assets/images/home-img-1.jpg'
 import homeImg2 from './../assets/images/home-img-2.jpg'
 import homeVideo from './../assets/images/video.mp4'
+import experinceImg from './../assets/images/experience.png'
 import Subtitle from '../shared/Subtitle'
 import sriLankaImg from './../assets/images/srilanka.png'
 import SearchBar from '../shared/SearchBar'
@@ -76,6 +77,46 @@ const Home = () => {
       </Container>
 
     </section>
+
+      {/* ------------Experience Section------------ */}
+
+      <section>
+        <Container>
+          <Row>
+            <Col lg="6">
+            <div className="experience__content">
+              <Subtitle subtitle={"Experience"}/>
+              <h2>With our all experience <br/> we will serve you</h2>
+              <p>Discover unforgettable moments on our curated tours.<br/> Immerse yourself in culture, adventure, and authentic experiences.<br/>
+                 Each journey is a unique chapter in your travel story,<br/> promising meaningful connections and cherished memories</p>
+            </div>
+
+            <div className='counter__wrapper d-flex align-items-center gap-5'>
+              <div className='counter__box'>
+                <span>5k+</span>
+                 <h6>Successful Trip</h6>
+              </div>
+
+              <div className='counter__box'>
+                <span>1k+</span>
+                 <h6>Regular Clients</h6>
+              </div>
+
+              <div className='counter__box'>
+                <span>4</span>
+                 <h6>Years experience</h6>
+              </div>
+
+            </div>
+            </Col>
+            <Col lg="6">
+            <div className="experince__img">
+              <img src={experinceImg} alt="" />
+            </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
 
     </>
   )
