bbedf1042dc89e3030bbc6d7d72a45523832bcab
idlakshan <id.lakshan21@gmail.com>
2023-12-24 12:12:05 +0530

    implemet tour details section

--- src/pages/TourDetails.jsx
+++ src/pages/TourDetails.jsx
@@ -1,20 +1,34 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import '../styles/tourDetails.css'
 import tourData from '../assets/data/tours'
 import { useParams } from 'react-router-dom'
-import { Col, Container, Row } from 'reactstrap'
+import { Col, Container, Form, ListGroup, Row } from 'reactstrap'
 import calculateAvgRating from '../utils/avgRating'
+import avatar from '../assets/images/avatar.jpg'
 
 const TourDetails = () => {
 
-  const { id } = useParams()
-  console.log(id);
+  const { id } = useParams();
+  const reviewMsgRef=useRef('')
+  const[tourRating,setTourRating]=useState(null)
+
+
+
 
   const tour = tourData.find((tour) => (tour.id === id))
 
   const { photo, title, desc, price, reviews, city, distance, maxGroupSize } = tour
 
-  const{totalRating,avgRating}=calculateAvgRating(reviews);
+  const { totalRating, avgRating } = calculateAvgRating(reviews);
+
+  const options={day:'numeric',month:'long',year:'numeric'};
+
+  const submitHandler=(e)=>{
+    e.preventDefault()
+    const reviewText=reviewMsgRef.current.value
+
+    alert(`${reviewText},${tourRating}`)
+  }
 
 
 
@@ -33,7 +47,7 @@ const TourDetails = () => {
                   <div className='d-flex align-items-center gap-5'>
 
                     <span className='tour__rating d-flex align-items-center gap-1'>
-                      <i class='ri-star-fill' style={{color:"var(--secondary-color)"}}></i>{avgRating === 0 ? null : avgRating}
+                      <i class='ri-star-fill' style={{ color: "var(--secondary-color)" }}></i>{avgRating === 0 ? null : avgRating}
                       {totalRating === 0 ? ('Not rated') : (<span>({reviews?.length})</span>)}
 
                     </span>
@@ -44,16 +58,67 @@ const TourDetails = () => {
                   </div>
 
                   <div className="tour__extra-details">
-                  <span> <i class='ri-map-pin-2-line'></i>{distance}km</span>
+                    <span> <i class='ri-map-pin-2-line'></i>{distance}km</span>
                     <span> <i class='ri-money-dollar-circle-line'></i>${price}/per person</span>
-                    <span> <i class='ri-group-line'></i>{maxGroupSize}</span>
+                    <span> <i class='ri-group-line'></i>{maxGroupSize} pepole</span>
                   </div>
                   <h5 >Description</h5>
                   <p>{desc}</p>
 
                 </div>
+
+                <div className='tour__reviews mt-4'>
+                  <h4>Reviews ({reviews?.length} reviews)</h4>
+
+
+
+                  <Form onSubmit={submitHandler} >
+
+                    <div className='d-flex align-items-center gap-3 mb-4 rating__group'>
+                      <span onClick={()=>setTourRating(1)}>1 <i class='ri-star-s-fill'></i></span>
+                      <span onClick={()=>setTourRating(2)}>2 <i class='ri-star-s-fill'></i></span>
+                      <span onClick={()=>setTourRating(3)}>3 <i class='ri-star-s-fill'></i></span>
+                      <span onClick={()=>setTourRating(4)}>4 <i class='ri-star-s-fill'></i></span>
+                      <span onClick={()=>setTourRating(5)}>5 <i class='ri-star-s-fill'></i></span>
+
+                    </div>
+
+                    <div className='review__input'>
+                         <input type="text" placeholder='share your thoughts' ref={reviewMsgRef} required/>
+                         <button className='btn primary__btn text-white' >Submit</button>
+                    </div>
+                  </Form>
+
+                  <ListGroup className='user__reviews'>
+                    {
+                      reviews?.map(review=>(
+                        <div className='review__item'>
+                            <img src={avatar} alt="" />
+                            <div className="w-100">
+                              <div className='d-flex align-items-center justify-content-between'>
+                                   <div>
+                                    <h5>lakshan</h5>
+                                    <p>{new Date('12-24-2023').toLocaleDateString("en-US",options)}</p>
+                                   </div>
+                                   <span className=' d-flex align-items-center '>
+                                          5 <i class='ri-star-s-fill'></i>
+                                   </span>
+                              </div>
+
+                              <h6>Amazing tour</h6>
+                            </div>
+                        </div>
+                      ))
+                    }
+
+                  </ListGroup>
+
+                </div>
               </div>
 
+            </Col>
+            <Col lg='4'>
+              
             </Col>
           </Row>
         </Container>
