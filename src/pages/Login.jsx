b1dc0c3271738bdc68633494f1dc99f53a62a359
idlakshan <id.lakshan21@gmail.com>
2023-12-24 22:30:54 +0530

    implement login section

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,63 @@
-import React from 'react'
+import React, { useState } from 'react'
+import '../styles/login.css'
+import loginImg from '../assets/images/login.png'
+import userIcon from '../assets/images/user.png'
+import { Button, Col, Container, Form, FormGroup, Row } from 'reactstrap'
+import { Link } from 'react-router-dom'
 
 const Login = () => {
+
+  const[credentials,setCredentials]=useState({
+   email:undefined,
+   password:undefined
+})
+
+  const handleChange=(e)=>{
+    setCredentials((pre)=>({
+     ...pre,[e.target.id]:e.target.value
+    })
+)}
+
+const handleClick=(e)=>{
+  e.preventDefault();
+
+}
   return (
-    <div>Login</div>
+    <section>
+      <Container>
+        <Row>
+          <Col lg='8' className='m-auto'>
+            <div className="login__container d-flex justify-content-between">
+              <div className='login__img'>
+                <img src={loginImg} alt="" />
+              </div>
+
+              <div className="login__form">
+                <div className='user'>
+                  <img src={userIcon} alt="" />
+
+                </div>
+                <h2>Login</h2>
+
+                <Form onSubmit={handleClick}>
+                  <FormGroup>
+                          <input type="text" placeholder='Email' required id='email' onChange={handleChange}/>
+                  </FormGroup>
+                  <FormGroup>
+                          <input type="password" placeholder='Password' required id='password' onChange={handleChange}/>
+                  </FormGroup>
+                  <Button className='btn secondary__btn auth__btn' type='submit'>Login</Button>
+                </Form>
+                <p>Don't have an account? <Link to='/regster'>Create</Link></p>
+
+              </div>
+
+            </div>
+          </Col>
+
+        </Row>
+      </Container>
+    </section>
   )
 }
 
